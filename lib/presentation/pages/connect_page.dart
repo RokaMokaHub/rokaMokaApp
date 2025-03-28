c3ee6d6aae38332c85c1535759232cafa3e9098c
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2025-03-27 22:52:59 -0300

    login_page

---
+++ lib/presentation/pages/connect_page.dart
@@ -0,0 +1,17 @@
+// lib/presentation/pages/connect_page.dart
+
+import 'package:flutter/material.dart';
+
+class ConnectPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Connect'),
+      ),
+      body: Center(
+        child: Text('Tela de Connect'),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/pages/login_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/backgroundLogin.png',
+            fit: BoxFit.none,
+            width: double.infinity,
+            height: double.infinity,
+            alignment: Alignment.topCenter,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(height: 20),
+                      Text(
+                        'Olá!',
+                        style: GoogleFonts.poppins(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFE94C19),
+                        ),
+                      ),
+                      Text(
+                        'A cada QR code, uma nova descoberta.',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          color: Color(0xFF9E2700),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        'Já tem uma conta?',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      // Botão "Acessar"
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 120, vertical: 14),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Color(0xFFB23F1A), Color(0xFFE94C19)],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/connect');
+                          },
+                          child: Center(
+                            child: Text(
+                              'Acessar',
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      Text(
+                        'Usuário novo?',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      // Botão "Cadastre-se"
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/signup');
+                        },
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Color(0xFFE94C19),
+                          padding: EdgeInsets.symmetric(horizontal: 127, vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          side: BorderSide(color: Color(0xFFE94C19)),
+                        ),
+                        child: Text(
+                          'Cadastre-se',
+                          style: GoogleFonts.poppins(
+                            color: Color(0xFFE94C19),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
