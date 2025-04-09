d2aba639678e8e3aeb2a958c9104073c7a94961f
apRodrigues00 <aprodrigues_10@hotmail.com>
2025-04-09 17:18:53 -0300

    chore: finalizado o form da página de edição perfil

--- lib/presentation/pages/edit_profile.dart
+++ lib/presentation/pages/edit_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roka_moka_app/presentation/widgets/password_text_field.dart';
 
 // ignore: use_key_in_widget_constructors
 class EditProfile extends StatefulWidget {
@@ -8,9 +9,25 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
-  
-  
-  
+  late TextEditingController _userNameController;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _userNameController = TextEditingController(text: 'XingLing Sakuma');
+    _emailController = TextEditingController(text: 'xingling@email.com');
+    _passwordController = TextEditingController(text: '123456');
+  }
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +35,104 @@ class _EditProfileState extends State<EditProfile> {
         top: false,
         bottom: false,
         child: Column(
-            
-        )
+          children: [
+            _appBar(context),
+            _buildStack(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 20,
+                children: [
+                  Text(
+                    'Editar perfil',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.left,
+                  ),
+                  _textField('Nome Completo', _userNameController),
+                  _textField('Email', _emailController),
+                  PasswordTextField(controller: _passwordController, placeholder: 'Senha'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
 
+Widget _textField(String placeholder, TextEditingController textController) {
+  return TextField(
+    controller: textController,
+    decoration: InputDecoration(
+      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+      hintText: placeholder,
+      hintStyle: TextStyle(color: Color(0xFFABABAB)),
+      suffixIcon: Icon(Icons.edit, color: Color(0xFFABABAB), size: 18.86),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+        borderSide: BorderSide(color: Color(0xFFABABAB), width: 2),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+        borderSide: BorderSide(color: Color(0xFFE94C19), width: 2),
+      ),
+    ),
+    style: TextStyle(height: 1.2),
+  );
+}
+
+Widget _buildStack() {
+  return Stack(
+    children: [
+      Column(
+        children: [
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              color: Color(0xFFB23F1A),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(80),
+                bottomRight: Radius.circular(80),
+              ),
+            ),
+          ),
+          Container(color: Colors.white),
+        ],
+      ),
+      Center(
+        child: Container(
+          width: 148,
+          height: 148,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Color(0xFFD9D9D9),
+            border: Border.all(color: Colors.white, width: 2),
+            image: DecorationImage(
+              image: AssetImage('./assets/images/user_icon.png'),
+              fit: BoxFit.none,
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _appBar(BuildContext context) {
+  return Container(
+    padding: EdgeInsets.only(top: 40, left: 16, right: 16),
+    decoration: BoxDecoration(color: Color(0xFFB23F1A)),
+    child: Row(
+      children: [
+        IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          splashColor: Colors.transparent,
+          iconSize: 30,
+        ),
+      ],
+    ),
+  );
 }
