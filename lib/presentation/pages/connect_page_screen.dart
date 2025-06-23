b0f74563464dd63f0087a4aaf2ea6f6ccc966596
Claudinei de Lima <claudinei@mertins.com>
2025-06-23 20:50:24 -0300

    add: ajustando o provider para adm

--- lib/presentation/pages/connect_page_screen.dart
+++ lib/presentation/pages/connect_page_screen.dart
@@ -91,7 +91,7 @@ class _ConnectPageState extends State<ConnectScreen> {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Login realizado com sucesso!')),
         );
-        context.read<UserProvider>().setRole(UserRole.curador);
+        context.read<UserProvider>().setRole(UserRole.administrador);
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (_) => const HomeController()),
