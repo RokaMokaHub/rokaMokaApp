0a7ed07a0e0e97f2cc9226e123a323a6cf9d3478
Bernardo <bernardo.f@inf.ufpel.edu.br>
2025-07-16 12:18:41 -0300

    Puxando os dados da obra

--- lib/constants/webservice.dart
+++ lib/constants/webservice.dart
@@ -1,6 +1,5 @@
 // SERVERS
-const String devServer = 'http://10.0.2.2:8080';
-
+const String devServer = 'http://192.168.1.111:8080';
 // LOGIN
 const String loginEndpoint = '$devServer/user/login';
 // RESET PASSWORD
@@ -9,4 +8,8 @@ const String resetPasswordEndpoint = '$devServer/user/reset-password';
 const String createUserEndpoint = '$devServer/user/normal/create';
 // CREATE ANON USER
 const String createAnonUserEndpoint = '$devServer/user/anonymous/create';
+
+// GET ARTWORK BY ID (gera a URL)
+String getArtworkByIdEndpoint(String artworkId) => '$devServer/artwork/$artworkId';
+
 
