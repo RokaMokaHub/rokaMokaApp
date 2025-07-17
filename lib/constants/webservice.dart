95e40964c5ec1767c3d0c386bcf69b2b85aa7f0e
Bernardo <bernardo.f@inf.ufpel.edu.br>
2025-07-17 16:48:29 -0300

    fix ip do server emulador

--- lib/constants/webservice.dart
+++ lib/constants/webservice.dart
@@ -1,5 +1,5 @@
 // SERVERS
-const String devServer = 'http://192.168.1.111:8080';
+const String devServer = 'http://10.0.2.2:8080';
 
 // LOGIN
 const String loginEndpoint = '$devServer/user/login';
